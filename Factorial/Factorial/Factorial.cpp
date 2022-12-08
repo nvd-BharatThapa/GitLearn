1612334bcb4073db606f80d9656d3f3a03d73506
Bharat <notveterandeveloper@gmail.com>
2022-12-08 22:25:01 +0530

    factorial program
    Added factoria.cpp

---
+++ Factorial/Factorial/Factorial.cpp
@@ -0,0 +1,22 @@
+// Program to find factorail
+
+#include <iostream>
+
+int fact(int num)
+{
+	int temp = num;
+	for (int i = 1; i < num; i++)
+	{
+		temp = temp * i;
+	}
+	return temp;
+}
+
+int main()
+{
+	std::cout << "Enter number: ";
+	int num{};
+	std::cin >> num;
+
+	std::cout << "Factorial of " << num << " is " << fact(num);
+}
